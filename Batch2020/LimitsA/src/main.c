ef7e53d0afb311b98fcda23cc90d53842518967f
Hardeep Singh <hardeep.rai@gmail.com>
2021-01-30 02:51:28 -0500

    char is signed int

---
+++ Batch2020/LimitsA/src/main.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+int main(int argc, char** argv)
+{
+    char current = 1, // current started with some convenient value
+        step = 1, // step size
+        maxValue, minValue, // Maximum and Minmum, required result
+        nextPositive = current + step, // next higher
+        nextNegative = current - step; // one step lower
+    
+    while(nextPositive - current == step)
+    {
+        current = nextPositive;
+        maxValue = nextPositive++;
+    }    
+    
+    current = 1;
+    while(current - nextNegative == step)
+    {
+        current = nextNegative;
+        minValue = nextNegative--;
+    }    
+    
+    printf("Min value %hhi\n", minValue);
+    printf("Max value %hhi\n", maxValue);
+
+    return 0;
+
+}
