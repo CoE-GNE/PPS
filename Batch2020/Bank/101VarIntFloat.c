a73fad97ffa93d8eaa054001cd2d7b3bfee41c09
Hardeep Singh <hardeep.rai@gmail.com>
2021-02-07 09:45:02 -0500

    Integer and float type variable

---
+++ Batch2020/Bank/101VarIntFloat.c
@@ -0,0 +1,14 @@
+// variable to type integer is declared and initialised with value 10
+
+int integerVariable = 10;
+
+// variable to type float is declared and initialised with value 11.1469
+
+float floatVariable = 11.1469;
+
+printf("\
+	\n\n\
+	Integer\t: %i\n\
+	Float\t: %f\n\
+	\n",
+	integerVariable, floatVariable);
